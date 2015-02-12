f5df00df7a17d625796a742bedbce211fd213c8d
LaurenAnn <LaurenAnn@192.168.14.217>
2015-02-12 08:35:48 -0700

    love

--- Monopoly/src/CCAC.java
+++ Monopoly/src/CCAC.java
@@ -6,11 +6,13 @@ public class CCAC extends Squares{
  	} 
  	public static void landedOnCommunityChest() 
 	{ 
- 		System.out.println(Intro.players.get(Intro.currentPlayer).getName() + " you landed on Community Chest."); 
+ 		System.out.println(Intro.players.get(Intro.currentPlayer).getName() + " you landed on Community Chest. You get 100 dollars. "); 
+ 		Intro.players.get(Intro.currentPlayer).setMoney(Intro.players.get(Intro.currentPlayer).getMoney() + 100);
  	} 
  	public static void landedOnChance() 
  	{ 
- 		System.out.println(Intro.players.get(Intro.currentPlayer).getName() + " you landed on Chance."); 
+ 		System.out.println(Intro.players.get(Intro.currentPlayer).getName() + " you landed on Chance. You get 100 dollars."); 
+ 		Intro.players.get(Intro.currentPlayer).setMoney(Intro.players.get(Intro.currentPlayer).getMoney() + 100);
  	} 
  
  } 
